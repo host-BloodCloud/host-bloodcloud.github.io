e1c5b54eedc4eaeaf3330e50f39688ab833df21f
host-BloodCloud <bloodcloudcontact@gmail.com>
2025-03-14 23:18:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
+const signInBtnLink = document.querySelector('.signInBtn-link');
+const signUpBtnLink = document.querySelector('.signUpBtn-link');
 const wrapper = document.querySelector('.wrapper');
-const loginLink = document.querySelector('.login-link');
-const registerLink = document.querySelector('.register-link');
-const btnPopup = document.querySelector('.btnLogin-popup');
-const iconClose = document.querySelector('.icon-close');
+signUpBtnLink.addEventListener('click', () => {
+    wrapper.classList.toggle('active');
+});
+signInBtnLink.addEventListener('click', () => {
+    wrapper.classList.toggle('active');
+});
