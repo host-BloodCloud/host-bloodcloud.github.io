e2d3e790de4070d3f571c3b44d04d8cf486c17b5
host-BloodCloud <bloodcloudcontact@gmail.com>
2025-03-15 15:46:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,9 @@
 const signInBtnLink = document.querySelector('.signInBtn-link');
 const signUpBtnLink = document.querySelector('.signUpBtn-link');
 const wrapper = document.querySelector('.wrapper');
-
 signUpBtnLink.addEventListener('click', () => {
     wrapper.classList.toggle('active');
 });
-
 signInBtnLink.addEventListener('click', () => {
     wrapper.classList.toggle('active');
-});
-
-// Login redirection
-document.addEventListener("DOMContentLoaded", function () {
-    const loginForm = document.querySelector(".sign-in form");
-
-    loginForm.addEventListener("submit", function (event) {
-        event.preventDefault(); // Prevent default form submission
-
-        // Redirect to home page
-        window.location.href = "https://host-bloodcloud.github.io/home";
-    });
 });
